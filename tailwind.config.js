792dcf8dbb6c2eb9a11196546e834ba289bda9e9
iftykhar alam <s.m.ifty49@gmail.com>
2025-03-24 11:44:02 +0600

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      padding: '20px'
+    },
+    extend: {
+      fontFamily :{
+        'Lato' :["Lato", "sans-serif"],
+        'josef' :["Josefin Sans", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
